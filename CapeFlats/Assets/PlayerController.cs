842bff43879b6277aa6543c058489274163ab944
Matthew Wierenga <matt.wierenga@outlook.com>
2024-08-02 10:29:51 +0200

    Trying a different way of coding it

--- CapeFlats/Assets/PlayerController.cs
+++ CapeFlats/Assets/PlayerController.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-
-    public static Grid Grid { get; private set; }
     // Start is called before the first frame update
     void Start()
     {
-        Grid = FindAnyObjectByType<Grid>();
         
     }
 
