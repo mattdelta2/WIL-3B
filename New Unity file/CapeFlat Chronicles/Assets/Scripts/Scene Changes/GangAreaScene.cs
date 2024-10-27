f5423e64baf46d3ab0b5a8ecc2550500853816bd
Matthew Wierenga <matt.wierenga@outlook.com>
2024-10-27 18:42:28 +0200

    Added SceneChange

--- New Unity file/CapeFlat Chronicles/Assets/Scripts/Scene Changes/GangAreaScene.cs
+++ New Unity file/CapeFlat Chronicles/Assets/Scripts/Scene Changes/GangAreaScene.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GangAreaScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string sceneName; // Set the name of the scene to load in the Inspector
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        // Check if the colliding object is the player
+        if (other.CompareTag("Player"))
+        {
+            // Load the specified scene
+            SceneManager.LoadScene(sceneName);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
