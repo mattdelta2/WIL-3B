1bdcb07d752513f72a6f8bb1135f2ab82f8a03de
Benzo00o <itzthewildboi@gmail.com>
2024-11-22 11:54:24 +0200

    started adding UI and things to main
    due to conflict errors when merging

---
+++ New Unity file/CapeFlat Chronicles/Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioMixer audioMixer;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Volume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("Master", volume);
+    }
+    public void MusicVolume(float volume)
+    {
+        audioMixer.SetFloat("Music", volume);
+    }
+    public void SFXVolume(float volume)
+    {
+        audioMixer.SetFloat("Effects", volume);
+    }
+
+}
